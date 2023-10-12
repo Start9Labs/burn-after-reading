7a345cce3a7241b3560d9ff73e0f4018a8176234
Matt Hill <mattnine@protonmail.com>
2023-10-12 15:48:33 -0600

    load live api for demo

--- frontend/src/app/services/api/api.service.factory.ts
+++ frontend/src/app/services/api/api.service.factory.ts
@@ -6,6 +6,7 @@ import { AuthStore } from '../auth.store'
 
 export function ApiServiceFactory (config: ConfigService, authStore: AuthStore): ApiService {
   switch (config.appType){
+    case 'demo':
     case 'live': return new LiveApi(authStore)
     case 'mock': return new MockApi()
   }
